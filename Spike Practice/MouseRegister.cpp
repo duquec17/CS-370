e5e062e32aa2b8ad5119f2e22e0f2a2d6168d3b9
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-02-03 22:02:26 -0500

    Update MouseRegister.cpp
    This is for the lightning function of our game. 30% matching what we desired. Still needs:
    - Limit cursor speed
    - Timer before another tile can be selected (maybe maintain grey tile)
    - Increase # of tiles changed per click

--- Spike Practice/MouseRegister.cpp
+++ Spike Practice/MouseRegister.cpp
@@ -1,514 +1,128 @@
 //Using SDL, SDL_image, standard IO, and strings
 #include <SDL.h>
-#include <SDL_image.h>
-#include <stdio.h>
-#include <string>
+#include <chrono>
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 1000;
-const int SCREEN_HEIGHT = 600;
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+const int TILE_SIZE = 32;
+const int ROWS = SCREEN_HEIGHT / TILE_SIZE;
+const int COLS = SCREEN_WIDTH / TILE_SIZE;
 
-//Button constants
-const int BUTTON_WIDTH = 250;
-const int BUTTON_HEIGHT = 149;
-const int TOTAL_BUTTONS = 16;
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
 
-enum LButtonSprite
-{
-	BUTTON_SPRITE_MOUSE_OUT = 0,
-	BUTTON_SPRITE_MOUSE_OVER_MOTION = 1,
-	BUTTON_SPRITE_MOUSE_DOWN = 2,
-	BUTTON_SPRITE_MOUSE_UP = 3,
-	BUTTON_SPRITE_TOTAL = 16
-};
+int tiles[ROWS][COLS] = { 0 };  // 2D array to represent tiles
+int hoverTilesRow = -1;
+int hoverTilesCol = -1;
+//std::chrono::time_point<std::chrono::steady_clock> hoverStartTime;
+std::chrono::time_point<std::chrono::steady_clock> clickStartTime;
 
-//Texture wrapper class
-class LTexture
-{
-public:
-	//Initializes variables
-	LTexture();
 
-	//Deallocates memory
-	~LTexture();
+void initSDL() {
+    // Initialize SDL
+    SDL_Init(SDL_INIT_VIDEO);
 
-	//Loads image at specified path
-	bool loadFromFile(std::string path);
+    // Create window
+    window = SDL_CreateWindow("2D Array Tiles Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
-#if defined(SDL_TTF_MAJOR_VERSION)
-	//Creates image from font string
-	bool loadFromRenderedText(std::string textureText, SDL_Color textColor);
-#endif
-
-	//Deallocates texture
-	void free();
-
-	//Set color modulation
-	void setColor(Uint8 red, Uint8 green, Uint8 blue);
-
-	//Set blending
-	void setBlendMode(SDL_BlendMode blending);
-
-	//Set alpha modulation
-	void setAlpha(Uint8 alpha);
-
-	//Renders texture at given point
-	void render(int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
-
-	//Gets image dimensions
-	int getWidth();
-	int getHeight();
-
-private:
-	//The actual hardware texture
-	SDL_Texture* mTexture;
-
-	//Image dimensions
-	int mWidth;
-	int mHeight;
-};
-
-//The mouse button
-class LButton
-{
-public:
-	//Initializes internal variables
-	LButton();
-
-	//Sets top left position
-	void setPosition(int x, int y);
-
-	//Handles mouse event
-	void handleEvent(SDL_Event* e);
-
-	//Shows button sprite
-	void render();
-
-private:
-	//Top left position
-	SDL_Point mPosition;
-
-	//Currently used global sprite
-	LButtonSprite mCurrentSprite;
-};
-
-//Starts up SDL and creates window
-bool init();
-
-//Loads media
-bool loadMedia();
-
-//Frees media and shuts down SDL
-void close();
-
-//The window we'll be rendering to
-SDL_Window* gWindow = NULL;
-
-//The window renderer
-SDL_Renderer* gRenderer = NULL;
-
-//Mouse button sprites
-SDL_Rect gSpriteClips[BUTTON_SPRITE_TOTAL];
-LTexture gButtonSpriteSheetTexture;
-
-//Buttons objects
-LButton gButtons[TOTAL_BUTTONS];
-
-LTexture::LTexture()
-{
-	//Initialize
-	mTexture = NULL;
-	mWidth = 0;
-	mHeight = 0;
+    // Create renderer
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 }
 
-LTexture::~LTexture()
-{
-	//Deallocate
-	free();
+void closeSDL() {
+    // Destroy window and renderer
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    // Quit SDL
+    SDL_Quit();
 }
 
-bool LTexture::loadFromFile(std::string path)
-{
-	//Get rid of preexisting texture
-	free();
+void drawTiles() {
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);  // Set draw color to white
 
-	//The final texture
-	SDL_Texture* newTexture = NULL;
+    for (int row = 0; row < ROWS; ++row) {
+        for (int col = 0; col < COLS; ++col) {
+           
+            //Variable for rectangle
+            SDL_Rect tileRect = { col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE};
 
-	//Load image at specified path
-	SDL_Surface* loadedSurface = IMG_Load(path.c_str());
-	if (loadedSurface == NULL)
-	{
-		printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
-	}
-	else
-	{
-		//Color key image
-		SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
+            //Draw Tile Outline
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            SDL_RenderDrawRect(renderer, &tileRect);
 
-		//Create texture from surface pixels
-		newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
-		if (newTexture == NULL)
-		{
-			printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
-		}
-		else
-		{
-			//Get image dimensions
-			mWidth = loadedSurface->w;
-			mHeight = loadedSurface->h;
-		}
+            //Draw Tile
+            if (tiles[row][col] == 1) {
+                //If tile is marked fill it with a different color
+                SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 
-		//Get rid of old loaded surface
-		SDL_FreeSurface(loadedSurface);
-	}
+                //Check to see if it's time to revert tile to color after 5 seconds
+                auto currentTime = std::chrono::steady_clock::now();
+                auto elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(currentTime - clickStartTime).count();
+                if (elapsedTime >= 3) {
+                    //Reverts color
+                    tiles[row][col] = 0;
+                }
+            } else if (row == hoverTilesRow && col == hoverTilesCol) {
+                //If mouse is hovering over tile fill with temporary grey color
+                SDL_SetRenderDrawColor(renderer, 169, 169, 169, 255);
 
-	//Return success
-	mTexture = newTexture;
-	return mTexture != NULL;
+            } else {
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            }
+
+            SDL_RenderFillRect(renderer, &tileRect);
+        }
+    }
 }
 
-#if defined(SDL_TTF_MAJOR_VERSION)
-bool LTexture::loadFromRenderedText(std::string textureText, SDL_Color textColor)
-{
-	//Get rid of preexisting texture
-	free();
+int main(int argc, char* args[]) {
+    initSDL();
 
-	//Render text surface
-	SDL_Surface* textSurface = TTF_RenderText_Solid(gFont, textureText.c_str(), textColor);
-	if (textSurface == NULL)
-	{
-		printf("Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError());
-	}
-	else
-	{
-		//Create texture from surface pixels
-		mTexture = SDL_CreateTextureFromSurface(gRenderer, textSurface);
-		if (mTexture == NULL)
-		{
-			printf("Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			//Get image dimensions
-			mWidth = textSurface->w;
-			mHeight = textSurface->h;
-		}
+    SDL_Event e;
+    bool quit = false;
 
-		//Get rid of old surface
-		SDL_FreeSurface(textSurface);
-	}
+    while (!quit) {
+        // Handle events
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+            else if (e.type == SDL_MOUSEBUTTONDOWN) {
+                // Mark the clicked tile
+                int mouseX, mouseY;
+                SDL_GetMouseState(&mouseX, &mouseY);
+                int clickedRow = mouseY / TILE_SIZE;
+                int clickedCol = mouseX / TILE_SIZE;
 
-	//Return success
-	return mTexture != NULL;
-}
-#endif
+                if (clickedRow >= 0 && clickedRow < ROWS && clickedCol >= 0 && clickedCol < COLS) {
+                    tiles[clickedRow][clickedCol] = 1;
 
-void LTexture::free()
-{
-	//Free texture if it exists
-	if (mTexture != NULL)
-	{
-		SDL_DestroyTexture(mTexture);
-		mTexture = NULL;
-		mWidth = 0;
-		mHeight = 0;
-	}
-}
+                    //Set the timer for reverting the color
+                    clickStartTime = std::chrono::steady_clock::now();
+                }
+            }
+            else if (e.type == SDL_MOUSEMOTION) {
+                //Update hoverTilesRow and hoverTilesCol
+                int mouseX, mouseY;
+                SDL_GetMouseState(&mouseX, &mouseY);
+                hoverTilesRow = mouseY / TILE_SIZE;
+                hoverTilesCol = mouseX / TILE_SIZE;
+            }
+        }
 
-void LTexture::setColor(Uint8 red, Uint8 green, Uint8 blue)
-{
-	//Modulate texture rgb
-	SDL_SetTextureColorMod(mTexture, red, green, blue);
-}
+        // Clear the renderer
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderClear(renderer);
 
-void LTexture::setBlendMode(SDL_BlendMode blending)
-{
-	//Set blending function
-	SDL_SetTextureBlendMode(mTexture, blending);
-}
+        // Draw tiles
+        drawTiles();
 
-void LTexture::setAlpha(Uint8 alpha)
-{
-	//Modulate texture alpha
-	SDL_SetTextureAlphaMod(mTexture, alpha);
-}
+        // Update the renderer
+        SDL_RenderPresent(renderer);
+    }
 
-void LTexture::render(int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip)
-{
-	//Set rendering space and render to screen
-	SDL_Rect renderQuad = { x, y, mWidth, mHeight };
+    closeSDL();
 
-	//Set clip rendering dimensions
-	if (clip != NULL)
-	{
-		renderQuad.w = clip->w;
-		renderQuad.h = clip->h;
-	}
-
-	//Render to screen
-	SDL_RenderCopyEx(gRenderer, mTexture, clip, &renderQuad, angle, center, flip);
-}
-
-int LTexture::getWidth()
-{
-	return mWidth;
-}
-
-int LTexture::getHeight()
-{
-	return mHeight;
-}
-
-LButton::LButton()
-{
-	mPosition.x = 0;
-	mPosition.y = 0;
-
-	mCurrentSprite = BUTTON_SPRITE_MOUSE_OUT;
-}
-
-void LButton::setPosition(int x, int y)
-{
-	mPosition.x = x;
-	mPosition.y = y;
-}
-
-void LButton::handleEvent(SDL_Event* e)
-{
-	//If mouse event happened
-	if (e->type == SDL_MOUSEMOTION || e->type == SDL_MOUSEBUTTONDOWN || e->type == SDL_MOUSEBUTTONUP)
-	{
-		//Get mouse position
-		int x, y;
-		SDL_GetMouseState(&x, &y);
-
-		//Check if mouse is in button
-		bool inside = true;
-
-		//Mouse is left of the button
-		if (x < mPosition.x)
-		{
-			inside = false;
-		}
-		//Mouse is right of the button
-		else if (x > mPosition.x + BUTTON_WIDTH)
-		{
-			inside = false;
-		}
-		//Mouse above the button
-		else if (y < mPosition.y)
-		{
-			inside = false;
-		}
-		//Mouse below the button
-		else if (y > mPosition.y + BUTTON_HEIGHT)
-		{
-			inside = false;
-		}
-
-		//Mouse is outside button
-		if (!inside)
-		{
-			mCurrentSprite = BUTTON_SPRITE_MOUSE_OUT;
-		}
-		//Mouse is inside button
-		else
-		{
-			//Set mouse over sprite
-			switch (e->type)
-			{
-			case SDL_MOUSEMOTION:
-				mCurrentSprite = BUTTON_SPRITE_MOUSE_OVER_MOTION;
-				break;
-
-			case SDL_MOUSEBUTTONDOWN:
-				mCurrentSprite = BUTTON_SPRITE_MOUSE_DOWN;
-				break;
-
-			case SDL_MOUSEBUTTONUP:
-				mCurrentSprite = BUTTON_SPRITE_MOUSE_UP;
-				break;
-			}
-		}
-	}
-}
-
-void LButton::render()
-{
-	//Show current button sprite
-	gButtonSpriteSheetTexture.render(mPosition.x, mPosition.y, &gSpriteClips[mCurrentSprite]);
-}
-
-bool init()
-{
-	//Initialization flag
-	bool success = true;
-
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
-		success = false;
-	}
-	else
-	{
-		//Set texture filtering to linear
-		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
-		{
-			printf("Warning: Linear texture filtering not enabled!");
-		}
-
-		//Create window
-		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (gWindow == NULL)
-		{
-			printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
-			success = false;
-		}
-		else
-		{
-			//Create vsynced renderer for window
-			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-			if (gRenderer == NULL)
-			{
-				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
-				success = false;
-			}
-			else
-			{
-				//Initialize renderer color
-				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-
-				//Initialize PNG loading
-				int imgFlags = IMG_INIT_PNG;
-				if (!(IMG_Init(imgFlags) & imgFlags))
-				{
-					printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
-					success = false;
-				}
-			}
-		}
-	}
-
-	return success;
-}
-
-bool loadMedia()
-{
-	//Loading success flag
-	bool success = true;
-
-	//Load sprites
-	if (!gButtonSpriteSheetTexture.loadFromFile("button.png"))
-	{
-		printf("Failed to load button sprite texture!\n");
-		success = false;
-	}
-	else
-	{
-		//Set sprites
-		for (int i = 0; i < BUTTON_SPRITE_TOTAL; ++i)
-		{
-			gSpriteClips[i].x = 0;
-			gSpriteClips[i].y = i * 200;
-			gSpriteClips[i].w = BUTTON_WIDTH;
-			gSpriteClips[i].h = BUTTON_HEIGHT;
-		}
-
-		//Set buttons in corners
-		//gButtons[0].setPosition(0, 0);
-		//gButtons[1].setPosition(SCREEN_WIDTH - BUTTON_WIDTH, 0);
-		//gButtons[2].setPosition(0, SCREEN_HEIGHT - BUTTON_HEIGHT);
-		//gButtons[3].setPosition(SCREEN_WIDTH - BUTTON_WIDTH, SCREEN_HEIGHT - BUTTON_HEIGHT);
-
-		//New set buttons everywhere
-		for (int i = 0; i < TOTAL_BUTTONS; i++) {
-			int row = i / 4;
-			int col = i % 4;
-			gButtons[i].setPosition(col * SCREEN_WIDTH / 4, row * SCREEN_HEIGHT / 4);
-		}
-	}
-
-	return success;
-}
-
-void close()
-{
-	//Free loaded images
-	gButtonSpriteSheetTexture.free();
-
-	//Destroy window	
-	SDL_DestroyRenderer(gRenderer);
-	SDL_DestroyWindow(gWindow);
-	gWindow = NULL;
-	gRenderer = NULL;
-
-	//Quit SDL subsystems
-	IMG_Quit();
-	SDL_Quit();
-}
-
-int main(int argc, char* args[])
-{
-	//Start up SDL and create window
-	if (!init())
-	{
-		printf("Failed to initialize!\n");
-	}
-	else
-	{
-		//Load media
-		if (!loadMedia())
-		{
-			printf("Failed to load media!\n");
-		}
-		else
-		{
-			//Main loop flag
-			bool quit = false;
-
-			//Event handler
-			SDL_Event e;
-
-			//While application is running
-			while (!quit)
-			{
-				//Handle events on queue
-				while (SDL_PollEvent(&e) != 0)
-				{
-					//User requests quit
-					if (e.type == SDL_QUIT)
-					{
-						quit = true;
-					}
-
-					//Handle button events
-					for (int i = 0; i < TOTAL_BUTTONS; ++i)
-					{
-						gButtons[i].handleEvent(&e);
-					}
-				}
-
-				//Clear screen
-				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-				SDL_RenderClear(gRenderer);
-
-				//Render buttons
-				for (int i = 0; i < TOTAL_BUTTONS; ++i)
-				{
-					gButtons[i].render();
-				}
-
-				//Update screen
-				SDL_RenderPresent(gRenderer);
-			}
-		}
-	}
-
-	//Free resources and close SDL
-	close();
-
-	return 0;
+    return 0;
 }
