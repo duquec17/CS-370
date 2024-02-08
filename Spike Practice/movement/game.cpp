5ea05c3cc717d094e7e14085cc8cacd042829c16
Eterna||solation <StarfleetCommand3@gmail.com>
2024-02-08 16:35:34 -0500

    Movement Update
    - Displays what tiles the player is in

--- Spike Practice/movement/game.cpp
+++ Spike Practice/movement/game.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 
 #include <SDL.h>
-#include <SDL2_gfxPrimitives.h>
 
 #include "game.h"
 #include "state.h"
 
 using namespace std;
 
+const int TILE_SIZE = 50;
+
 game_state::game_state(SDL_Renderer *rend) : state(rend) {
-	player.w = w / 10;
-	player.h = h / 10;
+	player.w = TILE_SIZE;
+	player.h = TILE_SIZE;
 	player.x = (w / 2) - (player.w/2);
 	player.y = (h / 2) - (player.h/2);
 
@@ -21,6 +22,12 @@ game_state::game_state(SDL_Renderer *rend) : state(rend) {
 	go_down = false;
 	go_left = false;
 	go_right = false;
+
+	for(int i = 0; i < 32; i++) {
+		for(int j = 0; j < 18; j++) {
+			tiles[i][j] = new tile(TILE_SIZE * i, TILE_SIZE * j);
+		}
+	}
 }
 
 game_state::~game_state() {
@@ -48,7 +55,16 @@ bool game_state::draw() {
 		player.x = constrain(player.x + 10, 0, w-player.w);
 	}
 
-	SDL_SetRenderDrawColor(rend, 0xFF, 0xFF, 0xFF, 0xFF);
+	// for(auto i : tiles)
+	// 	i->draw(rend, &player);
+
+	for(int i = 0; i < 32; i++) {
+		for(int j = 0; j < 18; j++) {
+			tiles[i][j]->draw(rend, &player);
+		}
+	}
+
+	SDL_SetRenderDrawColor(rend, 0xDD, 0xBB, 0xFF, 0xFF);
 	SDL_RenderFillRect(rend, &player);
 
 	return true;
@@ -109,6 +125,32 @@ bool game_state::handle_event(const SDL_Event &e) {
 			SDL_GetMouseState(&mx, &my);
 			break;
 		default: break;
+	}
+	return result;
+}
+
+tile::tile(int X_POS, int Y_POS) {
+	t.w = TILE_SIZE;
+	t.h = TILE_SIZE;
+	t.x = X_POS;
+	t.y = Y_POS;
+}
+bool tile::draw(SDL_Renderer *rend, SDL_Rect *player) {
+	SDL_SetRenderDrawColor(rend, 0xCC, 0xEE, 0xFF, 0xFF);
+	if(is_in(player)) {
+		SDL_SetRenderDrawColor(rend, 0x00, 0xFF, 0x00, 0xFF);
+	}
+	SDL_RenderFillRect(rend, &t);
+	return true;
+}
+bool tile::is_in(SDL_Rect *player) {
+	bool result = false;
+	if((player->x == t.x && player->y == t.y) ||
+		((player->x > t.x && player->x < (t.x + TILE_SIZE)) ||
+		((player->x + TILE_SIZE) > t.x && (player->x + TILE_SIZE) <= (t.x + TILE_SIZE))) &&
+		((player->y > t.y && player->y < (t.y+TILE_SIZE)) ||
+		((player->y + TILE_SIZE) > t.y && (player->y + TILE_SIZE) <= (t.y + TILE_SIZE)))) {
+			result = true;
 	}
 	return result;
 }
